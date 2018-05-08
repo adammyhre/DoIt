7b2526056b0f2a14b8feda90678f9e357a02c602
Adam Myhre <adam@undergalaxie.com>
2018-05-07 21:09:51 -0700

    Added table for first screen

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -2,22 +2,32 @@
 //  ViewController.swift
 //  DoIt
 //
-//  Created by Bradley Myhre on 2018-05-07.
+//  Created by Adam Myhre on 2018-05-07.
 //  Copyright © 2018 undergalaxie.com. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
     }
 
 
