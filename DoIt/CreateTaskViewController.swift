7145db3f48be9573b82a61ebaddbf137be57b3f9
Adam Myhre <adam@undergalaxie.com>
2018-05-08 08:47:03 -0700

    Setting things up

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Adam Myhre on 2018-05-08.
+//  Copyright © 2018 undergalaxie.com. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextfield: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
